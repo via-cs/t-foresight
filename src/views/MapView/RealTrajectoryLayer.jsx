ce36a8cfddc8beb58f0a5a418fbc7b5e7a3c2e57
吴江 <wujiang5521@qq.com>
2023-10-17 16:07:56 +0800

    Update the guidance for the first practice

--- src/views/MapView/RealTrajectoryLayer.jsx
+++ src/views/MapView/RealTrajectoryLayer.jsx
@@ -9,11 +9,18 @@ import {Layer} from "react-konva";
  * @constructor
  */
 function RealTrajectoryLayer({store, mapSize, scaleBalance}) {
-    // 1. You can get the trajectory of the selected player from store here
+    // 0. I have added some example files with some comments.
+    //    You can find the file list in README.md.
+
+    // 1. You can get the trajectory of the selected player from store here.
+    //    You can refer to store.playerPositions, where I get the current positions of all players.
+    //    But you need to get several future positions of the selected player.
     //    e.g., const trajectory = store.selectedPlayerTrajectory;
 
     // 2. You can render the trajectory here.
-    //    You may want to refer to https://konvajs.org/api/Konva.Arrow.html
+    //    You may want to refer to:
+    //    - how to draw a simple shape: https://konvajs.org/docs/react/Shapes.html and
+    //    - the API documentation of an arrow shape: https://konvajs.org/api/Konva.Arrow.html
     return <Layer>
 
     </Layer>
