93a4c708620fb4271c3b570b1116e95eaaa0bd57
ychen3221 <ychen3221@gatech.edu>
2024-01-12 00:33:05 -0500

    context-change

--- src/views/ContextView/AttentionItem.jsx
+++ src/views/ContextView/AttentionItem.jsx
@@ -1,13 +1,52 @@
+import {inject, observer} from "mobx-react";
 import {styled} from "@mui/material/styles";
 import {Box, Slider as MuiSlider, Typography} from "@mui/material";
 import {Check, Close} from "@mui/icons-material";
 
-function AttentionItem({colorLabel, label, value, attention, curAtt}) {
-    return <Container>
+function AttentionItem({store, colorLabel, label, value, attention, curAtt}) {
+    let selectAttention = [];
+    let isInclude = false;
+    if (store.viewedPrediction !== -1) {
+        const playerData = store.predictions[String(store.viewedPrediction)].attention;
+        const topThreeAvgsPerPlayer = {};
+        Object.keys(playerData).forEach(playerKey => {
+            if (playerData[playerKey]) {
+                const avgs = Object.values(playerData[playerKey])
+                .sort((a, b) => b - a); // 降序排序
+
+                // 取排序后的前三个值
+                topThreeAvgsPerPlayer[playerKey] = avgs.slice(0, 3);
+            }
+        });
+
+        if (store.playerNames[0].includes(String(label)) || store.playerNames[1].includes(String(label))) {
+            isInclude = true;
+            for (let i = 0; i < store.playerNames.length; i++) {
+                for (let j = 0; j < store.playerNames[i].length; j++) {
+                    if (store.playerNames[i][j] === String(label)) {
+                        selectAttention = topThreeAvgsPerPlayer['p'+i+j];
+                    }
+                }
+            }
+        } else if (String(label) === "Radiant") {
+            isInclude = true;
+            selectAttention = topThreeAvgsPerPlayer['t0'];
+        } else if (String(label) === "Dire") {
+            isInclude = true;
+            selectAttention = topThreeAvgsPerPlayer['t1'];
+        } else if (String(label) === "Environment") {
+            isInclude = true;
+            selectAttention = topThreeAvgsPerPlayer['g'];
+        }
+    }
+
+    return <div>
+    <Container>
         <Box width={20} height={20} flex={'0 0 20px'} display={'flex'} alignItems={'center'}>{colorLabel}</Box>
         <Box width={150} flex={'0 0 150px'}><Typography sx={{maxWidth: 150}} align={'center'}
                                                         noWrap>{label}</Typography></Box>
         <Box width={100} flex={'0 0 120px'}>
+            {isInclude && value === undefined && <Typography sx={{maxWidth: 150}} align={'center'} noWrap>{"Overall Attention"}</Typography>}
             {typeof value === 'number' && <Typography noWrap>{value.toFixed(0)}</Typography>}
             {typeof value === 'string' && <Typography noWrap>{value}</Typography>}
             {typeof value === 'boolean' && <Typography noWrap>{value ? <Check/> : <Close/>}</Typography>}
@@ -27,23 +66,48 @@ function AttentionItem({colorLabel, label, value, attention, curAtt}) {
             {attention && attention.length && <BarContainer>
                 <Bar style={{
                     width: `${attention[0] * 100 / 3}%`,
-                    opacity: (attention[0] - 0.5) * 2
+                    opacity: (attention[0])
                 }}/>
                 <Bar style={{
                     width: `${attention[1] * 100 / 3}%`,
-                    opacity: (attention[1] - 0.5) * 2
+                    opacity: (attention[1])
                 }}/>
                 <Bar style={{
                     width: `${attention[2] * 100 / 3}%`,
-                    opacity: (attention[2] - 0.5) * 2,
+                    opacity: (attention[2]),
                     borderRadius: '0 4px 4px 0'
-                }}/>
+                }}/> 
             </BarContainer>}
         </Box>
     </Container>
+    {selectAttention.length === 3 && <Container>
+        <Box width={156} height={20} ></Box>
+        <Box width={200} flex={'0 0 120px'}><Typography sx={{maxWidth: 150}} align={'center'}
+                                                        noWrap>{'Worker\'s Attention'}</Typography></Box>
+        <Box flex={1} position={'relative'}>
+            {selectAttention && selectAttention.length && <BarContainer>
+                <Bar style={{
+                    width: `${selectAttention[0] * 100 / 3}%`,
+                    opacity: (selectAttention[0])
+                }}/>
+                <Bar style={{
+                    width: `${selectAttention[1] * 100 / 3}%`,
+                    opacity: (selectAttention[1])
+                }}/>
+                <Bar style={{
+                    width: `${selectAttention[2] * 100 / 3}%`,
+                    opacity: (selectAttention[2]),
+                    borderRadius: '0 4px 4px 0'
+                }}/> 
+            </BarContainer>}
+        </Box>
+        </Container>}
+    </div>
 }
 
-export default AttentionItem;
+export default inject('store')(observer(AttentionItem));
+
+
 
 const Container = styled('div')({
     display: 'flex',
@@ -57,7 +121,7 @@ const Slider = styled(MuiSlider)(({theme}) => ({
     height: 15,
     '& .MuiSlider-track': {
         border: 'none',
-        borderRadius: 0,
+        borderRadius: 5,
     },
     '& .MuiSlider-thumb': {
         width: 4,
@@ -65,28 +129,31 @@ const Slider = styled(MuiSlider)(({theme}) => ({
         shadow: `0 0 4px 4px ${theme.palette.background.paper}`,
     },
     '& .MuiSlider-rail': {
-        backgroundColor: theme.palette.background.paper,
+        backgroundColor: 'grey',
         borderRadius: 0,
     },
     '& .MuiSlider-mark': {
         backgroundColor: theme.palette.background.paper,
-        height: 30,
-        width: 1,
+        height: 20,
+        width: 2,
     }
 }))
 
+
 const Anchor = styled('div')(({theme}) => ({
     position: 'absolute',
-    width: 0,
-    height: 21,
     transition: 'left .3s ease',
     transform: 'translateX(-50%)',
-    top: 10,
+    top: '12%',
     borderTop: `3px solid ${theme.palette.secondary.main}`,
     borderBottom: `3px solid ${theme.palette.secondary.main}`,
     borderLeft: `3px solid white`,
     borderRight: `3px solid white`,
-}))
+    width: '10px', // 增加锚点的宽度
+    height: '25px', // 增加锚点的高度
+    backgroundColor: theme.palette.secondary.main, // 改变锚点的背景颜色以便更加突出
+}));
+
 
 const BarContainer = styled('div')(({theme}) => ({
     position: 'relative',
