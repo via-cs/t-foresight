3256556ede1f3a863e27a2324fbff892cb7c34e2
吴江 <wujiang5521@qq.com>
2024-01-23 15:12:28 -0800

    Fix bug: the image will not display sometimes

--- src/components/KonvaImage.jsx
+++ src/components/KonvaImage.jsx
@@ -1,21 +1,21 @@
 import {useCallback, useEffect, useRef} from "react";
 import {Image} from 'react-konva';
 
-function useImage(src, onlyNonTransparent, onLoad) {
+function useImage(src, onLoad) {
     const image = useRef(new window.Image());
     useEffect(() => {
-        image.current.src = src;
         image.current.addEventListener('load', onLoad);
         return () => image.current.removeEventListener('load', onLoad);
+    }, [onLoad]);
+    useEffect(() => {
+        image.current.src = src;
     }, [src, onLoad]);
     return image.current;
 }
 
 function KonvaImage({x, y, w, h, src}) {
     const imageNode = useRef(null);
-    const redraw = useCallback(() => {
-        imageNode.current && imageNode.current.getLayer().batchDraw();
-    }, []);
+    const redraw = useCallback(() => imageNode.current && imageNode.current.getLayer().batchDraw(), []);
     const image = useImage(src, redraw);
 
     return <Image x={x} y={y}
